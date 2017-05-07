57db8149f5600027d0e38756099b25ac93db8f62
Jan van Brügge <supermanitu@gmail.com>
2017-05-07 14:48:18 +0200

    Reformat Problem 23

--- 016/Main.hs
+++ 016/Main.hs
@@ -1,8 +1,9 @@
 module P16 where
 
 dropEvery :: [a] -> Int -> [a]
-dropEvery l n =
-    let helper [] b _ = b
+dropEvery l n = reverse $ helper l [] n
+    where
+        helper :: [a] -> [a] -> Int -> [a]
+        helper [] b _ = b
         helper (x:xs) b 1 = helper xs b n
-        helper (x:xs) b c = helper xs (x:b) (c-1) in
-    reverse $ helper l [] n
+        helper (x:xs) b c = helper xs (x:b) (c-1)
